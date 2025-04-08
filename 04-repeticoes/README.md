b5e91926a9633bc71197ac1056d13fd40f822b8f
Nathanael Vieira <tmntsplinter04@gmail.com>
2025-04-07 23:09:36 -0300

    Solve all problems from While Loop

--- 04-repeticoes/README.md
+++ 04-repeticoes/README.md
@@ -7,4 +7,34 @@ Aqui terá os exercícios propostos durante as aulas de Python.
 **4.1** Dados uma sequência de 5 números inteiros. Calcule a soma de todos os números da sequência. <br>
     Resolva usando apenas duas variáveis:
 
+<br>
+
 **4.2** Dado um número inteiro positivo *n*, escreva um algoritmo que imprime a tabuada de *n* até o valor 10. <br>
+
+<br>
+
+**4.3** Escreva um algoritmo que dados um número interio porsitivo *n*, imprime na tela todos os númeors de 1 a *n*.
+
+<br>
+
+**4.4** Escreva um programa que dadas duas notas de 0 a 10 calcula a média aritmética entre elas. <br>
+    *OBS: Criar um validador que não permite notas fora do intervalo*
+
+<br>
+
+**4.5** Escreva um programa que dado um inteiro *n* positivo, calcula e imprime a soma de todos os números <br>
+    interiros entre 1 e *n*.
+
+<br>
+
+**4.6** Escreva um algoritmo que recebe um inteiro prositivo *num* e imprime todo os divisores positivos de *num*. <br>
+    *Exemplo: Suponha que $num = 28$, nessa situação devemos imprimir os números 1, 2, 4, 7, e 28, que todos os* <br>
+    *divisores do 28.*
+
+<br>
+
+---
+
+## Lista de Exercício 3
+
+1. 
--- 04-repeticoes/problemas.py
+++ 04-repeticoes/problemas.py
@@ -1,2 +1,122 @@
-for i in range(6):
-    print(i)
+# 4.1
+
+# print('\n-- Soma de 5 números inteiros --')
+# sum = 0
+
+# # # Usando o For:
+# # for i in range(5):
+# #     num = int(input('Digite um número: '))
+# #     sum += num
+
+# # Usando o While:
+# i = 0
+# while i < 5:
+#     num = int(input('Digite um número: '))
+#     sum += num
+#     i += 1
+
+
+# print(f'A soma é {sum}.\n')
+
+
+
+# 4.2
+
+# print('\n-- Tabuada de um número --')
+
+# num = int(input('Digite o número que deseja mostrar a tabuada: '))
+
+# Usando o While:
+# i = 1
+# while i <= 10:
+#     print(f'{num} x {i:2} = {num * i}')
+#     i += 1
+
+# # # Usando For:
+# # for i in range(1, 11):
+# #     print(f'{num} x {i:2} = {num * i}')
+
+
+
+# # 4.3
+
+# print('\n-- Mostra N números na tela --')
+
+# num = int(input('Digite um número inteiro: '))
+
+# # Usando o While:
+# # i = 1
+# # while i <= num:
+# #     print(i)
+# #     i += 1
+
+# # Usando o For:
+# for i in range(1, num+1):
+#     print(i)
+
+
+
+# 4.4
+
+# print('\n-- Média de 2 notas com validação --')
+
+# sum = 0
+
+# note = float(input('Digite a 1ª Nota: '))
+
+# while note < 0 or note > 10:
+#     note = float(input('Digite uma nota válida! Digite a 1ª Nota: '))
+
+# sum += note
+
+# note = float(input('Digite a 2ª Nota: '))
+
+# while note < 0 or note > 10:
+#     note = float(input('Digite uma nota válida! Digite a 2ª Nota: '))
+
+# sum += note
+
+# print(f'A média será {sum / 2:.2}\n')
+
+
+
+# 4.5
+
+# print('\n-- Calcula a soma dos n primeiros números --')
+
+# num = int(input('Digite um número: '))
+
+# while num <= 0:
+#     num = int(input('Digite um número positivo: '))
+
+# sum = 0
+# i = 1
+
+# # Usando o For:
+# # for i in range(1,num+1):
+# #     sum += i
+
+# # Usando o While:
+# while i <= num:
+#     sum += i
+#     i += 1
+
+# print(f'A soma será {sum}.\n')
+
+
+
+# 4.6
+
+print('\n-- Divisores de um número --')
+
+num = int(input('Digite um número inteiro: '))
+
+i = 1
+while i <= num:
+    if num % i == 0:
+        if num != i:
+            print(f'{i}', end=', ')
+        else:
+            print(f'{i}\n')
+    i += 1
+
