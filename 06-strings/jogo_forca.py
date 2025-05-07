57d851818749b6136a6ba8957f2df2b88c378546
Nathanael Vieira <tmntsplinter04@gmail.com>
2025-05-07 16:00:11 -0300

    Hangman

--- 06-strings/jogo_forca.py
+++ 06-strings/jogo_forca.py
@@ -43,12 +43,49 @@ while erros < 7 and p_misteriosa != p_resp:
         print(tentativas_erradas)
         erros += 1
 
-    # print(f'Opção 2: {p_misteriosa != p_correta}')
     print(f"'{p_misteriosa}'")
-    # print(f"'{p_resp}'")
     print(f'Erros: {erros}/7')
     print()
 
 print('=' * 30)
 print('Fim')
 print(f'A Resposta é: {p_correta}\n')
+
+
+
+## Resolução do Professor
+
+# #incializa o jogo
+# erros = 0
+# letras_chutadas = " "
+# #sorteia a palavra
+# palavra = "The Shawshank Redemption"
+# segredo = ''
+
+# for c in palavra:
+#     if c == ' ':
+#         segredo = segredo + '  '
+#     else:
+#         segredo = segredo + '_ '
+
+# while erros < 6 and "_" in segredo:
+#     print(segredo)
+#     segredo = ""
+#     print(f"Erros: {erros}")
+#     print(f'Letras chutadas: {letras_chutadas}')
+#     letra = input("Letra: ")
+#     letras_chutadas = letras_chutadas + letra.lower()
+
+#     if not letra.lower() in palavra.lower():
+#         erros = erros + 1
+
+#     for c in palavra.lower():
+#         if c in letras_chutadas:
+#             segredo = segredo + c + " "
+#         else:
+#             segredo = segredo + "_ "
+
+# if erros >= 6:
+#     print(f"Voce perdeu, a palavra era {palavra}")
+# else:
+#     print(f"Parabéns, você descobriu a palavra {palavra}")
