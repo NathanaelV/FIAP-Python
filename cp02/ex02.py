30b60e37c090433981afebe9410647e93e5ccb42
Nathanael Vieira <tmntsplinter04@gmail.com>
2025-05-12 22:36:04 -0300

    Exercise 02 solved

--- cp02/ex02.py
+++ cp02/ex02.py
@@ -2,3 +2,18 @@ banknote = int(input('Digite o valor da nota a ser trocada: '))
 first_coin = int(input('Digite o valor da primeira moeda: '))
 second_coin = int(input('Digite o valor da segunda moeda: '))
 
+first_count = 0
+second_count = 0
+
+while banknote > 0:
+    if banknote % second_coin == 0:
+        second_count = banknote // second_coin
+        banknote = 0
+    else:
+        banknote -= first_coin
+        first_count += 1
+
+if banknote == 0:
+    print(f'{first_count} moeda(s) de {first_coin} e {second_count} moeda(s) de {second_coin}')
+else:
+    print('Não é possível fazer a troca')
