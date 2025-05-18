0e31742c0c236433bfe278b1e1350f08ade7e6e5
Nathanael Vieira <tmntsplinter04@gmail.com>
2025-05-18 15:01:47 -0300

    CP03: Solved exercise 01

--- cp03/ex01.py
+++ cp03/ex01.py
@@ -1,3 +1,24 @@
 product = 'something'
+highest_price = -float('inf')
+highest_added_value = -float('inf')
+name_price = ''
+name_value = ''
+
 while product != '':
-    input(f'Digite o produto: ')
+    product = input('Digite o produto: ')
+    if product != '':
+        quantity = int(input('Quantidade: '))
+        price = float(input('Preço: '))
+
+        if highest_price < price:
+            name_price = product
+            highest_price = price
+        
+        if highest_added_value < price * quantity:
+            name_value = product
+            highest_added_value = price * quantity
+
+print('\nResultado:')
+print(f'O {name_price} é o produto com maior preço. R$ {highest_price}')
+print(f'O {name_value} é o produto com maior valor agregado. R$ {highest_added_value}')
+
